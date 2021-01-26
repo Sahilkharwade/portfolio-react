c1f2c29d59f7c54eb721bda44506fdfcfe489970
Sahilkharwade <skharwade.smk@gmail.com>
2021-01-26 11:44:24 +0530

    React routing done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Resume from './components/Resume';
+import Project from './components/Project';
 function App() {
   return (
-    <div className='App'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-3'>
-            <Sidebar />
-          </div>
-          <div className='col-lg-9 app_main-content'>
-            <Navbar />
+    <BrowserRouter>
+      <div className='App'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-3'>
+              <Sidebar />
+            </div>
+            <div className='col-lg-9 app_main-content'>
+              <Navbar />
+              <switch>
+                <Route exact path='/'>
+                  <About />
+                </Route>
+                <Route exact path='/resume'>
+                  <Resume />
+                </Route>
+                <Route exact path='/project'>
+                  <Project />
+                </Route>
+              </switch>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   const [active, setActive] = useState('About');
+  /*
+    here the problem is that when we directly put url as project it show about 
+    as here in usestate we use about as default but project is when user goes to project and
+    directly from url then this happen so we have to use url for our active state 
+    so we use useEffect hooks for that 
+  
+  */
+ useEffect(() => {
+   let currentUrl = window.location.href;
+   if(currentUrl.endsWith('/'))
+    setActive('About')
+    else if(currentUrl.endsWith('/project'))
+    setActive('Project')
+    else if(currentUrl.endsWith('/resume'))
+    setActive('Resume')
+ }, [active])
 
   return (
     <div className='navbar'>
       <div className='navbar__active'>{active}</div>
       <div className='navbar__items'>
         {active !== 'About' && (
+          <Link to="/">
           <div className='navbar__item' onClick={() => setActive('About')}>
             About
           </div>
+          </Link>
+          
         )}
         {active !== 'Resume' && (
+
+          <Link to='/resume'>
+
+          
           <div className='navbar__item' onClick={() => setActive('Resume')}>
             Resume
           </div>
+          </Link>
         )}
 
         {active !== 'Project' && (
+          <Link to='/project'>
           <div className='navbar__item' onClick={() => setActive('Project')}>
             Project
           </div>
+          </Link>
         )}
       </div>
     </div>
